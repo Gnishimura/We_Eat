ef04c35d2f1d38cf21b6cbc8a30b01974ddb5eff
Gabriel Nishimura <gabe.nishimura@gmail.com>
2018-11-29 13:56:21 -0800

    add find_top_recs fxn

--- recommender.py
+++ recommender.py
@@ -7,11 +7,20 @@ def average_user_ratings(restaurant_matrix, user_ratings_dict):
     and the restaurant (item matrix), and returns the user's average ratings for each feature """
     ratings = restaurant_matrix.copy()
     for key, value in user_ratings_dict.items():
-        ratings.loc[key] *= value
+        ratings.loc[restaurant_matrix['alias'] == key] *= value
     return ratings.mean()
 
-def update_user_ratings(user, user_matrix, average_ratings):
+def update_user_ratings(user, user_matrix, restaurant_matrix, user_ratings_dict):
     """Input user name as string, user_matrix pd dataframe, and average ratings from average_user_ratings,
     ouput an updated user matrix"""
-    user_matrix.loc[user] = average_ratings
+    average_rats = average_user_ratings(restaurant_matrix, user_ratings_dict)
+    user_matrix.loc[user] = average_rats
+
+
+def find_top_recs(user, n, ratings_matrix):
+    """Input user alias, number of recs, and the ratings matrix.  Output a sorted series"""
+    unweighted_prefs = ratings_matrix.loc[user]
+    weighted = unweighted_prefs * np.log(popularity + 1)
+    return weighted.sort_values(ascending=False)[:n]
+
 

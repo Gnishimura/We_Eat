b4bb169642c442c0d6b8fda42ceed7630ad7e3f7
Gabriel Nishimura <gabe.nishimura@gmail.com>
2018-11-29 14:24:53 -0800

    standardize_ratings fxn

--- recommender.py
+++ recommender.py
@@ -8,19 +8,26 @@ def average_user_ratings(restaurant_matrix, user_ratings_dict):
     ratings = restaurant_matrix.copy()
     for key, value in user_ratings_dict.items():
         ratings.loc[restaurant_matrix['alias'] == key] *= value
-    return ratings.mean()
+    average_ratings = ratings.mean()
+    return average_ratings
+
+def standardize_ratings(average_ratings):
+    return (average_ratings - average_ratings.mean())/average_ratings.std
 
 def update_user_ratings(user, user_matrix, restaurant_matrix, user_ratings_dict):
     """Input user name as string, user_matrix pd dataframe, and average ratings from average_user_ratings,
     ouput an updated user matrix"""
     average_rats = average_user_ratings(restaurant_matrix, user_ratings_dict)
-    user_matrix.loc[user] = average_rats
+    standardized_rats = standardize_ratings(average_rats)
+    user_matrix.loc[user] = standardized_rats
 
 
-def find_top_recs(user, n, ratings_matrix):
-    """Input user alias, number of recs, and the ratings matrix.  Output a sorted series"""
+def find_top_recs(user, n, ratings_matrix, popularity_scores):
+    """Input user alias, number of recs, ratings matrix, and popularity series.  Output a sorted series"""
     unweighted_prefs = ratings_matrix.loc[user]
-    weighted = unweighted_prefs * np.log(popularity + 1)
+    weighted = unweighted_prefs * np.log(popularity_scores + 1)
     return weighted.sort_values(ascending=False)[:n]
 
-
+def standardize_ratings(user_matrix):
+    user_matrix.loc(user) / user_matrix.loc(user).mean()
+    

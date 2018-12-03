3e0cab058d047cc9c7c348e3e45384c767f36a55
Gabriel Nishimura <gabe.nishimura@gmail.com>
2018-12-03 12:00:57 -0800

    data to play around with

---
+++ survey_results.py
@@ -0,0 +1,79 @@
+
+    
+gabe = {'purple-café-and-wine-bar-seattle-3': 6,
+ 'metropolitan-grill-seattle': 6,
+ 'mod-pizza-seattle-7': 8,
+ 'mikes-noodle-house-seattle': 4,
+ 'specialtys-café-and-bakery-seattle-34': 7,
+ 'tamarind-tree-seattle': 7,
+ 'japonessa-sushi-cocina-seattle': 9,
+ 'el-borracho-seattle': 8,
+ 'chan-seattle-seattle-2': 8,
+ 'vons-1000-spirits-seattle-4': 4,
+ 'red-robin-gourmet-burgers-seattle-7': 7,
+ 'the-pink-door-seattle-4': 3,
+ 'harbor-city-restaurant-seattle': 8,
+ 'wild-ginger-seattle': 8,
+ 'zig-zag-cafe-seattle-2': 4,
+ 'daawat-grill-seattle-2': 8,
+ 'gopoké-seattle-5': 6,
+ 'café-campagne-seattle-3': 7}
+
+nicole = {'the-pink-door-seattle-4': 6,
+                          'harbor-city-restaurant-seattle': 9,
+                          'japonessa-sushi-cocina-seattle': 9,
+                          'metropolitan-grill-seattle': 5,
+                          'el-borracho-seattle': 10,
+                          'café-campagne-seattle-3': 7,
+                          'mod-pizza-seattle-7': 8,
+                          'specialtys-café-and-bakery-seattle-34': 9,
+                          'vons-1000-spirits-seattle-4': 5,
+                          'tamarind-tree-seattle': 7}
+
+alex = {'the-pink-door-seattle-4': 5,
+                          'harbor-city-restaurant-seattle': 6,
+                          'japonessa-sushi-cocina-seattle': 5,
+                          'metropolitan-grill-seattle': 4,
+                          'el-borracho-seattle': 4,
+                          'café-campagne-seattle-3': 5,
+                          'mod-pizza-seattle-7': 5,
+                          'specialtys-café-and-bakery-seattle-34': 7,
+                          'vons-1000-spirits-seattle-4': 2,
+                          'tamarind-tree-seattle': 5}
+
+jordan = {'the-pink-door-seattle-4': 4,
+                          'harbor-city-restaurant-seattle': 8,
+                          'japonessa-sushi-cocina-seattle': 9,
+                          'metropolitan-grill-seattle': 8,
+                          'el-borracho-seattle': 7,
+                          'café-campagne-seattle-3': 4,
+                          'mod-pizza-seattle-7': 8,
+                          'specialtys-café-and-bakery-seattle-34': 5,
+                          'vons-1000-spirits-seattle-4': 4,
+                          'tamarind-tree-seattle': 7}
+
+jonny = {'the-pink-door-seattle-4': 5,
+                          'harbor-city-restaurant-seattle': 8,
+                          'japonessa-sushi-cocina-seattle': 9,
+                          'metropolitan-grill-seattle': 6,
+                          'el-borracho-seattle': 7,
+                          'café-campagne-seattle-3': 7,
+                          'mod-pizza-seattle-7': 8,
+                          'specialtys-café-and-bakery-seattle-34': 8,
+                          'vons-1000-spirits-seattle-4': 4,
+                          'tamarind-tree-seattle': 6}
+
+grant = {'the-pink-door-seattle-4': 7,
+                          'harbor-city-restaurant-seattle': 2,
+                          'japonessa-sushi-cocina-seattle': 9,
+                          'metropolitan-grill-seattle': 2,
+                          'el-borracho-seattle': 4,
+                          'café-campagne-seattle-3': 6,
+                          'mod-pizza-seattle-7': 8,
+                          'specialtys-café-and-bakery-seattle-34': 5,
+                          'vons-1000-spirits-seattle-4': 4,
+                          'tamarind-tree-seattle': 5}
+
+survey_results = [gabe, nicole, jordan, jonny, alex, grant]
+
+usernames = ['gabe', 'nicole', 'jordan', 'jonny', 'alex', 'grant']

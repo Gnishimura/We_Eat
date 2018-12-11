2c810b8defa46d71ca15ca85d8405e9e4f8c5b64
Gabriel Nishimura <gabe.nishimura@gmail.com>
2018-12-11 10:32:08 -0800

    added rec fxn

--- webapp/app.py
+++ webapp/app.py
@@ -1,48 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
-# from .ALS_recommender import ALSRecommender
+from pymongo import MongoClient
 
-# import pickle
+from .ALS_recommender import ALSRecommender
+
+import pickle
+
+we_eat_client = MongoClient()
+we_eat_database = we_eat_client['we_eat']
 
 app = Flask(__name__, static_url_path='')
 
-# with open('data/item_factors_df.pkl', 'rb') as f:
-#     item_factors = pickle.load(f)
+with open('data/item_factors_df.pkl', 'rb') as f:
+    item_factors = pickle.load(f)
 
-# with open('data/inv_alias_dict.pickle', 'rb') as g:
-#     inv_alias_dict = pickle.load(g)
+with open('data/inv_alias_dict.pickle', 'rb') as g:
+    inv_alias_dict = pickle.load(g)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# @app.route('/login')
-# def login():
-#     """Takes a username and makes it a key of the survey_results dictionary"""
-#     data = request.json
-#     pass
 
-# @app.route('/vote', methods=['GET', 'POST'])
-# def vote():
-#     """Compiles a dictionary of survey results and makes that dict a value 
-#     (corresponding to the username key above) in the survey_results dictionary"""
+@app.route('/survey', methods=['POST'])
+def survey():
+    data = request.get_json()
+    survey_collection = we_eat_database['surveys']
+    survey_collection.insert_one(data)
+    print(data)
+    return f"received data for {data['user']}"
 
-#     data = request.json
-#     login()
-#     survey_results["Gabe"] = request["user_data"]
-    
 
-# @app.route('/recommend', methods=['GET', 'POST'])
-# def recommend():
-#     """Return a recommendation, given two usernames."""
-#     data = request.json
-#     recommender = ALSRecommender(item_factors, inv_alias_dict)
-#     single_user_survey_results = recommender.get_preds_df_from_survey(#insert survey results from website here)
-#     recommender.add_to_preds_df(single_user_survey_results)
-#     rec = recommender.get_a_rec('gabe', 'nicole')
+@app.route('/recommend', methods=['GET', 'POST'])
+def recommend(user1, user2):
+    """Return a recommendation, given two usernames."""
+    data = request.get_json()
+    user1 = survey_collection.find_one({'user': user1})
+    user2 = survey_collection.find_one({'user': user2})
+    recommender = ALSRecommender(item_factors_df, inv_alias_dict)
+    user1_df = recommender.user_preds_from_survey(user1)
+    user2_df = recommender.user_preds_from_survey(user2)
+    preds_df = recommender.compile_df(user1_df, user2_df)
+    rec = recommender.get_a_rec(user1, user2, preds_df)
+    return jsonify({'go eat HERE!': rec})
 
-#     return jsonify({'go eat HERE!': rec})
+
+
 
 
 if __name__ == '__main__':

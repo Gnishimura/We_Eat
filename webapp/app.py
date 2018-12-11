eb65aa5b8f7d225b2a53bbc07912192b4cf42807
Gabriel Nishimura <gabe.nishimura@gmail.com>
2018-12-11 11:17:01 -0800

    update webapp

--- webapp/app.py
+++ webapp/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from pymongo import MongoClient
 
-from .ALS_recommender import ALSRecommender
+from ..ALS_recommender import ALSRecommender
 
 import pickle
 
@@ -32,18 +32,32 @@ def survey():
     return f"received data for {data['user']}"
 
 
-@app.route('/recommend', methods=['GET', 'POST'])
-def recommend(user1, user2):
+@app.route('/recommend', methods=['POST'])
+def recommend():
     """Return a recommendation, given two usernames."""
     data = request.get_json()
+    #TODO(Gabe): get user 1 and user 2 from data
     user1 = survey_collection.find_one({'user': user1})
     user2 = survey_collection.find_one({'user': user2})
     recommender = ALSRecommender(item_factors_df, inv_alias_dict)
     user1_df = recommender.user_preds_from_survey(user1)
     user2_df = recommender.user_preds_from_survey(user2)
-    preds_df = recommender.compile_df(user1_df, user2_df)
-    rec = recommender.get_a_rec(user1, user2, preds_df)
+    compiled_df = recommender.compile_df(user1_df, user2_df)
+    rec = recommender.get_a_rec(user1, user2, compiled_df)
     return jsonify({'go eat HERE!': rec})
+
+@app.route('/recommend/<user1>/<user2>')
+def recommend(user1, user2):
+    """Return a recommendation, given two usernames."""
+    user1 = survey_collection.find_one({'user': user1})
+    user2 = survey_collection.find_one({'user': user2})
+    recommender = ALSRecommender(item_factors_df, inv_alias_dict)
+    user1_df = recommender.user_preds_from_survey(user1)
+    user2_df = recommender.user_preds_from_survey(user2)
+    compiled_df = recommender.compile_df(user1_df, user2_df)
+    rec = recommender.get_a_rec(user1, user2, compiled_df)
+    return jsonify({'go eat HERE!': rec})
+
 
 
 
